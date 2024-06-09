467aabcac40ba76e0f86c1bf859a0cc26c98bdbc
sulpap <sdi2000162@di.uoa.gr>
2024-06-09 13:14:20 +0300

    upd

--- HW2/jobCommander/correct_syntax.c
+++ HW2/jobCommander/correct_syntax.c
@@ -2,5 +2,5 @@
 
 // gives the user instructions on running the program
 const char *correct_syntax() {
-  return "jobCommander/bin/jobCommander [serverName] [portNum] [jobCommanderInputCommand]\nValid commands: 'issueJob', 'setConcurrency', 'stop', 'poll' and 'exit'\n";
+  return "./bin/jobCommander [serverName] [portNum] [jobCommanderInputCommand]\nValid commands: 'issueJob', 'setConcurrency', 'stop', 'poll' and 'exit'\n";
 }
